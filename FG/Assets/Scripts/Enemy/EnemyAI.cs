bffcd43e1d9b3845b8aa53ba7ee305f904bc9233
FleepyBeepy <PGreen1@student.fullsail.edu>
2024-01-12 22:49:39 -0600

    Added empty ai enemy scripts and created a simple model as a wall

---
+++ FG/Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+
+    
+    void Start()
+    {
+        //Start model in idle animation
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
